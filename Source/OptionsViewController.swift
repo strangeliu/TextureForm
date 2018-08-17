0426523fd856c8ec6e790f9de7025a40fff42a7c
strangeliu <11201334@qq.com>
2018-08-17 17:52:03 +0800

    Introducing OptionsViewController (developing)

---
+++ Source/OptionsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  OptionsViewController.swift
+//  TextureForm
+//
+//  Created by Liu on 2018/8/15.
+//  Copyright © 2018年 grace.app. All rights reserved.
+//
+
+import Foundation
+import AsyncDisplayKit
+
+public protocol OptionType: Equatable, CustomStringConvertible {}
+
+open class OptionsViewController<Option: OptionType>: FormViewController {
+    
+    private(set) var options: [Option]
+    
+    var selectedOption: Option?
+    public var onSelectOption: ((Option) -> Void)?
+    
+    private let optionRows: [TextRow]
+    
+    public init(options: [Option], selected: Option? = nil) {
+        self.options = options
+        self.selectedOption = selected
+        let optionRows = options.map({ option in
+            return TextRow(title: option.description)
+        })
+        self.optionRows = optionRows
+        super.init()
+        
+        for (index, row) in optionRows.enumerated() {
+            row.onSelected = { [weak self] row in
+                row.accessoryType = .checkmark
+                let option = options[index]
+                self?.onSelectOption?(option)
+            }
+        }
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override func buildForm() -> [Section] {
+        return [Section(rows: optionRows)]
+    }
+    
+    
+    
+    
+    
+}
+
